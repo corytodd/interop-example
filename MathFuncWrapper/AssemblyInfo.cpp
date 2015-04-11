2de60340496559ee7733f355431e11cf5c26f8cf
Cory Todd <corneliustodd@gmail.com>
2015-04-11 16:26:12 -0700

    remove unused files add comments

--- MathFuncWrapper/AssemblyInfo.cpp
+++ MathFuncWrapper/AssemblyInfo.cpp
@@ -16,7 +16,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
 [assembly:AssemblyProductAttribute(L"MathFuncWrapper")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2015")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Cory Todd 2015")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
--- MathFuncWrapper/MathFuncWrapper.cpp
+++ MathFuncWrapper/MathFuncWrapper.cpp
@@ -4,12 +4,25 @@
 
 #include "stdafx.h"
 
+// Include the header to our native library. Not that you
+// MUST add BOTH the reference to the MathFuncDll project AND
+// add the path $(SolutionDir)MathFuncDl to the Additional Include Directories 
+// property for this project. That property is found under 
+// Properties -> Configuration Properties -> C/C++ -> General
 #include "MathFuncDll.h"
 
 namespace MathFuncWrapper
 {
+	///
+	/// Implements the wrapper functionality. Exposes exported functions
+	/// in MathFuncDll as wrapped functions that are consumable from C#.
+	/// Since these are all simple primitives we do not need to convert any values.
+	/// If we were using String types, there would be some CLR legwork to get the 
+	/// string properly marshalled between the native and C# code.
+	///
 	public ref class WrappedMathFunc
 	{
+	
 	public:
 		double AddWrapper(double a, double b) {
 			return MathFuncs::MyMathFuncs::Add(a, b);
@@ -22,6 +35,9 @@ namespace MathFuncWrapper
 			return MathFuncs::MyMathFuncs::Multiply(a, b);
 		}
 
+
+		// Note that this native code will throw an exception if b == 0
+		// and I'm not entirely sure why we don't see that exception here.
 		double DivideWrapper(double a, double b) {
 			return MathFuncs::MyMathFuncs::Divide(a, b);
 		}
